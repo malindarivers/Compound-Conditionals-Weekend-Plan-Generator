7face757bc5fd02ab3f6af206c4fe395449230af
malindarivers <43344616+malindarivers@users.noreply.github.com>
2019-04-11 09:49:53 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 
 function whatToDo(dayOfWeek, weather) {
-     if(dayOfWeek === "saturday"&& weather ==="sunny"){
-    $("#result").html("I'll go for a bike ride.");
-   }
     
-    if(dayOfWeek === "saturday" && weather==="cloudy"){
-    $("#result").html("I'll go binge watch Netflix.");
-    }
-    
-    if(dayOfWeek==="sunday" && weather==="sunny"){
+    if (dayOfWeek === "saturday" && weather === "sunny") {
+        $("#result").html("I'll go for a bike ride.");
+        
+    } else if(dayOfWeek === "saturday" && weather==="cloudy") {
+        $("#result").html("I'll go binge watch Netflix.");
+        
+    } else if(dayOfWeek==="sunday" && weather==="sunny") {
         $("#result").html("I'll go to the park.");
+        
+    } else if (dayOfWeek === "sunday" && weather === "cloudy") {
+        $("#result").html("I'll go binge watch Netflix.");
+        
+} else { $("#result").html("I'll stay inside");
 }
 
-    if(dayOfWeek==="sunday" && weather==="cloudy"){
-    $("#result").html("I'll go binge watch Netflix.");
+    
 }
-
-else{"I'll stay inside"}
-
-
-
-} 
 
 $("button").click(function(){
     var dayOfWeek = $("#dayOfWeek").val();
